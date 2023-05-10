1abcfcf215958cbbc6338291b85dc58c3c877bef
naseokju <lukas95@naver.com>
2023-05-10 15:47:19 +0900

    7.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "42b1dc84b3aff38d2a29e1a4f1bc2112";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie { // IGetMoviesResult의 results
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    overview: string;
+}
+
+export interface IGetMoviesResult {
+    dates: {
+        maximum: string;
+        minimun: string;
+    };
+    page: number;
+		results: IMovie[]; // IMovie의 배열
+    total_pages: number;
+    total_results: number;
+}
+
 export function getMovies() {
     return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`)
         .then((response) => response.json()
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,6 @@
-import "styled-components"
+import "styled-components";
 
-declare module "styled-componets" {
+declare module "styled-components" {
     export interface DefaultTheme {
         red: string;
         black: {
